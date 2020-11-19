66e60ebc5636850ddee39dc442b489d120aec95d
interpreted-validator-6256 <danielle.leppertsimenauer@gmail.com>
2020-11-19 22:24:26 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  element = array.pop
+end
+
+def pop_with_args(array)
+  elements = array.pop(2)
+end
+
+def using_shift(array)
+  element = array.shift
+end
+
+def shift_with_args(array)
+  elements = array.shift(2)
+end
